430cc89cb82f6d7a3db23062a5c1f3016419691a
Yvon Momboisse <momboisse.yvn@gmail.com>
2022-03-12 09:39:15 +0100

    "feat: improved error handling for missing exchange   rates"

--- js/portfolio.js
+++ js/portfolio.js
@@ -16,14 +16,27 @@ class Portfolio {
       return money.amount;
     }
     let key = money.currency + "->" + currency;
+    let rate = exchangeRates.get(key);
+    if (rate == undefined) {
+      return undefined;
+    }
     return money.amount * exchangeRates.get(key);
   }
 
   evaluate(currency) {
+    let failures = [];
     let total = this.moneys.reduce((sum, money) => {
+      let convertedAmount = this.convert(money, currency);
+      if (convertedAmount == undefined) {
+        failures.push(money.currency + "->" + currency);
+        return sum;
+      }
       return sum + this.convert(money, currency);
     }, 0);
-    return new Money(total, currency);
+    if (failures.length == 0) {
+      return new Money(total, currency);
+    }
+    throw new Error("Missing exchange rate(s):[" + failures.join() + "]");
   }
 }
 
